258f87cf68c1e9cb73fc80f8085774bcd85ed1e8
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-04 19:11:44 -0800

    finish out first draft of business logic with main object and methods.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,34 @@
+function diceRoll() {
+  return Math.floor((Math.random() * 6) + 1);
+}
 //Business Logic
 
+function GameState() {
+  this.score = [0,0];
+  this.player = 0;
+  this.turnScore = 0;
+  this.diceResult = 0;
+};
+
+GameState.prototype.switchPlayers = function() {
+  this.turnScore = 0;
+  this.player += 1;
+  this.player = this.player%2;
+}
+
+GameState.prototype.rollDice = function() {
+  this.diceResult = diceRoll();
+  if (diceRoll === 1) {
+    this.switchPlayers();
+  } else {
+    this.turnScore += this.diceResult;
+    if ((this.score[this.player] + this.turnScore) > 100) {
+      return ("Winner " + this.player + "!");
+    }
+  }
+}
+
+GameState.prototype.hold = function() {
+  this.score[this.player] += this.turnScore;
+  this.switchPlayers();
+}
