d760392f0725c0dcb586a55ad531981077a13b1e
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-07 22:33:53 -0800

    finish out UI Logic for js and test that all features are displaying properly.

--- js/scripts.js
+++ js/scripts.js
@@ -9,22 +9,25 @@ function GameState() {
   this.player = 0;
   this.turnScore = 0;
   this.diceResult = 0;
+  this.rollHistory = [];
 };
 
 GameState.prototype.switchPlayers = function() {
   this.turnScore = 0;
   this.player += 1;
   this.player = this.player%2;
+  this.rollHistory = [];
 }
 
 GameState.prototype.rollDice = function() {
   this.diceResult = diceRoll();
-  if (this.diceResult <= 1) {
+  if (this.diceResult === 1) {
     this.switchPlayers();
   } else {
     this.turnScore += this.diceResult;
+    this.rollHistory.push(this.diceResult);
     if ((this.score[this.player] + this.turnScore) > 100) {
-      return ("Winner " + this.player + "!");
+      return ("The Winner Is " + this.player + "!");
     }
   }
 }
@@ -37,24 +40,27 @@ GameState.prototype.hold = function() {
 //UI Logic
 let gameState = new GameState();
 
-function handleRoll(event) {
-  event.preventDefault();
+function handleRoll() {
   gameState.rollDice();
+  let rollScore = gameState.rollHistory;
+  console.log(gameState.diceResult);
+  if (gameState.player === 0) {
+    document.querySelector("span#player1Roll").innerText = rollScore;
+  } else if (gameState.player === 1) {
+    document.querySelector("span#player2Roll").innerText = rollScore;
+  }
 };
 
 function handleHold() {
   gameState.hold();
   let scoreTotal = gameState.score;
-  let span = document.getElementById("player1Score");
   console.log(scoreTotal);
-  if (this.player === 0) {
-    span.append(scoreTotal);
-  } else if (this.player === 1){
-    document.getElementById("player2Score").innerText.value= scoreTotal;
-  }
+  document.querySelector("span#player1Score").innerText = scoreTotal[0];
+  document.querySelector("span#player2Score").innerText = scoreTotal[1];
 };
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function(e) {
+  e.preventDefault();
   const newGameBtn = document.getElementsByClassName("new-game");
   const rollBtn = document.getElementById("roll");
   const holdBtn = document.getElementById("hold");
@@ -70,3 +76,57 @@ window.addEventListener("load", function() {
 //   } else {
 //     document.querySelector("div#game-window").innerText= null;
 //   }
+
+
+/*let gameState = new GameState();
+
+function handleRoll(event) {
+  event.preventDefault();
+  document.getElementById("dice-value").innerText = null;
+  document.querySelector("span#current-score").innerText = null;
+  gameState.rollDice();
+  console.log("dice result" + gameState.diceResult)
+  console.log("roll total" + gameState.turnScore)
+  const rollingTotal = gameState.turnScore;
+  const diceAmount = gameState.diceResult;
+  document.getElementById("dice-value").innerText = diceAmount;
+  document.getElementById("current-score").innerText = rollingTotal;
+  let scoreTotal1 = gameState.score[0];
+  let scoreTotal2 = gameState.score[1];
+  if (rollingTotal === 0) {
+    if (gameState.player === 1){
+      document.getElementById("score-p1").innerText= scoreTotal1;
+    } else if (gameState.player === 0){
+      document.getElementById("score-p2").innerText= scoreTotal2;
+    }
+  }
+  
+};
+
+function handleHold() {
+  gameState.hold();
+  document.querySelector("span#current-score").innerText = null;
+  document.getElementById("dice-value").innerText = null;
+  let scoreTotal1 = gameState.score[0];
+  let scoreTotal2 = gameState.score[1];
+  console.log("score Total: " + scoreTotal1);
+
+  console.log("player object: " + gameState.player);
+  if (gameState.player === 1){
+    document.getElementById("score-p1").innerText= scoreTotal1;
+  } else if (gameState.player === 0){
+    document.getElementById("score-p2").innerText= scoreTotal2;
+  }
+};
+
+window.addEventListener("load", function() {
+  // const newGameBtn = document.getElementsByClassName("new-game");
+  // const gameRulesBtn = document.getElementsByClassName("how-to");
+  // const closeBtn = document.getElementsByClassName("close-how");
+  const rollBtn = document.getElementById("roll");
+  const holdBtn = document.getElementById("hold");
+
+  rollBtn.addEventListener("click", handleRoll);
+
+  holdBtn.addEventListener("click", handleHold);
+}); */
